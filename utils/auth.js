6122077e8e2dc077c76e520550655692ecc16c85
upennbootcamp23 <143010411+upennbootcamp23@users.noreply.github.com>
2024-01-08 21:11:10 -0500

    Added the utils/auth.js file

---
+++ utils/auth.js
@@ -0,0 +1,9 @@
+let withAuthorize = (req, res, next) => {
+    if(!req.session.user_id) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuthorize;
