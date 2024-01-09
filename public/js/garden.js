82c842588030b78c82eba79e28f6102a782b38ca
jeffch19 <17jcummings@gmail.com>
2024-01-08 21:22:44 -0500

    adding api info in main.js, api.js, and garden.js

---
+++ public/js/garden.js
@@ -0,0 +1,12 @@
+// Handles garden-related functionality
+
+
+// Process the species list data
+export function processSpeciesList(data) {
+  return data;
+}
+
+//process the species details
+export function processSpeciesDetails(data){
+  return data;
+}
