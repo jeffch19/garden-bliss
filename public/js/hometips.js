d06e4f2d84e908a64127a56f964327d88bbcebc2
Brad <Bradrockk@gmail.com>
2024-01-14 06:09:55 -0500

    started rework of hometips.js

--- public/js/hometips.js
+++ public/js/hometips.js
@@ -1,21 +1,17 @@
-// const tips = require("")
+// temp vars for testing.
+const tiptext = "Hello!"
+const Username = "Someone"
 
-const schedule = require('node-schedule');
-const count = tips.length
-console.log(count)
-// use math.random to decide what tips will be shown. filter out to make sure it is not the same tip as last day.
-function decidetip(){
-let num = Math.floor((Math.random() * tips.length));
-NewTip = tips[num];
-console.log(NewTip);
-return NewTip
+
+
+function displaytip(){
+// define the elements
+let tip = document.getElementById("dailyTip");
+let UserCred = document.getElementById("userName");
+// replace the elements inner value with the appropriate data.
+tip.innerText = tiptext
+UserCred.innerText = (`From User: ${Username}`)
 }
 
-decidetip()
 
-// display a new tip every 24h.
-
-const job = schedule.scheduleJob({hour:23, minute: 30}, function(){
-    
-  console.log("hello!")
-  });
+displaytip()
