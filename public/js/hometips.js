1c49779320e13b2da03de25364a5ca785cf1e718
Brad <Bradrockk@gmail.com>
2024-01-14 10:41:44 -0500

    tip can now be displayed via JSON. Needs to be fetched from tipsData.json still.

--- public/js/hometips.js
+++ public/js/hometips.js
@@ -1,17 +1,22 @@
-// temp vars for testing.
-const tiptext = "Hello!"
-const Username = "Someone"
-
+// temp var for testing.
+const rawTip =  {
+  "user_name": "DaisyDiva",
+  "title": "Daisy Care",
+  "content": "Deadheading daisies can greatly increase their blooming duration. Regularly check for wilted flowers."
+}
 
 
 function displaytip(){
-// define the elements
+// define the elements.
 let tip = document.getElementById("dailyTip");
 let UserCred = document.getElementById("userName");
+// create tipText and UserName text to display.
+const tipData = Object.values(rawTip)
+tipText = tipData[2]
+UserName = tipData[0]
 // replace the elements inner value with the appropriate data.
-tip.innerText = tiptext
-UserCred.innerText = (`From User: ${Username}`)
+tip.innerText = tipText
+UserCred.innerText = (`From User: ${UserName}`)
 }
-
 
 displaytip()
