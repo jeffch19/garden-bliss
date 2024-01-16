4e83a3bf604f846a884cabce214f07d42b6fe5dd
docloc1 <docloc1@powell.com>
2024-01-15 21:13:26 -0500

    Adding stuff

--- seeds/seed.js
+++ seeds/seed.js
@@ -4,9 +4,6 @@ const { User, Plant, Tip, Post, Comment } = require('../models');
 const userData = require('./userData.json');
 
 const postData = require('./postData.json');
-=======
-
-
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
